f91144a3aa261dd02a3e0e5b49ee691b7f621e2f
David Lara <oscar.lr97@gmail.com>
2018-06-27 15:26:51 -0700

    small changes

--- src/aes.js
+++ src/aes.js
@@ -1,3 +1,7 @@
 export default class AES{
-    
+    //TODO: Crear metodos
+    //TODO:Estructurar clase
+    //TODO: encriptar, desencriptar
+    //TODO: 4 procesos principales
+    //TODO: Key Expansion
 }
