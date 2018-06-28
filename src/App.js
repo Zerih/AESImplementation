8cf036480498e66cba96ed4f33af21ef21525031
David Lara <oscar.lr97@gmail.com>
2018-06-28 10:35:52 -0700

    conversions addded

--- src/App.js
+++ src/App.js
@@ -1,2 +1,22 @@
-import aes from './aes';
-import personalAlg from './personalAlg';
+import AES from './aes.js';
+//import personalAlg from './personalAlg';
+ const cifrar = () => {
+    let text = document.getElementById("text").value
+    console.log(text);
+    //por mientras
+    let textInBytes = aes.textToBytes(text);
+    console.log("bytes: "+textInBytes);
+
+    let textInHex = aes.hexFromBytes(textInBytes);
+    console.log("hex: "+textInHex);
+
+
+    let originalText = aes.hexToBytes(textInHex);
+    originalText = aes.textFromBytes(originalText);
+    console.log("original text: "+originalText);
+    }
+const aes = new AES();
+document.getElementById('cipher').addEventListener('click', cifrar);
+
+
+
